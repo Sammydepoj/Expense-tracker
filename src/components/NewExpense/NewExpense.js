5a1daeb8d8f748450748afacf85756e649414cd6
Sammydepoj <samueladepoju86@yahoo.com>
2023-01-27 18:41:28 +0100

    first commit

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import ExpenseForm from './ExpenseFom';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+
+    const saveExpenseDataHandler =(enteredExpenseData)=>{
+        const expenseData = {
+            ...enteredExpenseData, 
+            id:Math.random().toString()
+        }
+        props.onAddExpense(expenseData);
+    };
+
+  return (
+    <div className='new-expense'>
+      <ExpenseForm onsaveExpenseData = {saveExpenseDataHandler} />
+    </div>
+  );
+};
+export default NewExpense;
