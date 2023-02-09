aaf5046496c3c5281f2b1d0770d4d4c813f0bc77
Sammydepoj <samueladepoju86@yahoo.com>
2023-02-09 18:02:31 +0100

    data moved to firebase realtime database

--- src/App.js
+++ src/App.js
@@ -1,42 +1,72 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
 
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const DUMMY_EXPENSES = [
   {
-    id: 'e1',
-    title: 'Toilet Paper',
+    id: "e1",
+    title: "Toilet Paper",
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: 'e3',
-    title: 'Car Insurance',
+    id: "e3",
+    title: "Car Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
+    id: "e4",
+    title: "New Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
   },
 ];
 
 function App() {
-const [expenses, setExpenses]= useState(DUMMY_EXPENSES);
+  // const [expensesData, setExpensesData] = useState([]);
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      const response = await fetch(
+        "https://react-http-dff7f-default-rtdb.firebaseio.com/expenses.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const responseData = await response.json();
+      // console.log(responseData);
+      const loadedExpenses = [];
+      
+      for (const key in responseData) {
+        // function formatDate(formattedDate) {
+        //   formattedDate = new Date();
+        // }
+        loadedExpenses.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+          date: responseData[key].date,
+        });
 
-const addExpenseHandler = (expense) => {
-  setExpenses(prevExpenses => {
-    return [expense, ...prevExpenses]
-  }); 
-};
+      }
+      // setExpensesData(loadedExpenses)
+      console.log(loadedExpenses);
+    };
+    fetchExpenses();
+  }, []);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
-      <NewExpense onAddExpense ={addExpenseHandler} />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
